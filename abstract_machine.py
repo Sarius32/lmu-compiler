98f182fa4eda835f643401f63ae70aed4895785e
Moritz Gärtner <moritzgaertner516@gmail.com>
2025-02-15 14:26:39 +0100

    python abstract machine parsing of object code and main loop started
    Instructions implemented: RST, INC, HLT

---
+++ abstract_machine.py
@@ -0,0 +1,126 @@
+from inspect import get_annotations as get_expect_args
+
+
+class AbstractMachine:
+    _storemax = 1_000_000
+    _store = [None] * _storemax
+
+    P, T, B = 0, 0, 0
+
+    def __init__(self, file_path: str):
+        # init translation of instructions
+        self._instr_dict = {
+            "RST": self._reset,
+            "LOD": ...,
+            "STO": ...,
+            "INC": self._inc,
+            "LIT": ...,
+            "JMP": ...,
+            "JOT": ...,
+            "JOF": ...,
+            "CAL": ...,
+            "RET": ...,
+            "OPR": ...,
+            "REA": ...,
+            "WRI": ...,
+            "HLT": None
+        }
+
+        try:
+            with open(file_path) as file:
+                code = file.readlines()
+        except OSError:
+            print(f"Error occurred during reading file at location: {file_path}")
+            exit(-1)
+
+        self._convert_to_exec(code)
+
+    def _convert_to_exec(self, code: list[str]):
+        self.code = []
+
+        for row, line in enumerate(code):
+            line = line.strip()
+            if line == "":
+                continue
+
+            instr_tuple = line.split(" ")
+            if len(instr_tuple) > 3:
+                print(f"Malformed code found in OBJECT code at")
+                print(f"    line {str(row).rjust(4)}:    {line}")
+                exit(-1)
+
+            instr, args = instr_tuple[0], instr_tuple[1:]
+            instr_func = self._instr_dict.get(instr, -1)
+            if instr_func == -1:
+                print(f"Unknown instruction '{instr}' found in OBJECT code at")
+                print(f"    line {str(row).rjust(4)}:    {line}")
+                print(f"               ‾‾‾{"^" * len(instr)}‾‾‾")
+                exit(-1)
+
+            if instr_func is None:
+                self.code.append(None)
+                if row != len(code) - 1:
+                    print(f"Instruction 'HLT' found before EOF in OBJECT code at")
+                    print(f"    line {str(row).rjust(4)}:    {line}")
+                    exit(-1)
+                else:
+                    return
+
+            arg_dict = get_expect_args(instr_func)
+            num_args, arg_types = len(arg_dict), list(arg_dict.values())
+            if len(args) > num_args:
+                len_args = len(line.replace(instr + " ", ""))
+                print(
+                    f"Too many arguments for instruction '{instr}' ({num_args} expected, {len(args)} found) found in OBJECT code at")
+                print(f"    line {str(row).rjust(4)}:    {line}")
+                print(f"               {" " * len(instr)} ‾‾‾{"^" * len_args}‾‾‾")
+
+                exit(-1)
+            if len(args) < num_args:
+                print(
+                    f"Too few arguments for instruction '{instr}' ({num_args} expected, {len(args)} found) found in OBJECT code at")
+                print(f"    line {str(row).rjust(4)}:    {line}")
+                exit(-1)
+
+            conv_args = []
+            for idx, (arg, type_to_be) in enumerate(zip(args, arg_types)):
+                # type is just one type
+                if type(type_to_be) == type:
+                    try:
+                        conv_args.append(type_to_be(arg))
+                    except ValueError:
+                        print(
+                            f"Error occurred during argument conversion ({type_to_be.__name__} expected, {type(arg).__name__} found) in OBJECT code at")
+                        print(f"    line {str(row).rjust(4)}:    {line}")
+                        space_len = len(instr) + 1 + ((len(args[0]) + 1) if idx == 1 else 0)
+                        print(f"               {" " * space_len}‾‾‾{"^" * len(arg)}‾‾‾")
+                        exit(-1)
+                # type is something like ["<", ">", ...]
+                else:
+                    ...  # TODO
+
+            self.code.append((instr_func, conv_args))
+
+    def _reset(self):
+        self.B = 0
+        self._store[self.B + 0] = 0
+        self._store[self.B + 1] = 0
+        self._store[self.B + 2] = 0
+        self.T = self.B+2
+
+    def _inc(self, i: int):
+        self.T += i
+
+    def run(self):
+        i = self.code[self.P]
+
+        while i is not None:
+            self.P += 1
+            instruction, args = i
+            instruction(*args)
+            i = self.code[self.P]
+
+
+if __name__ == "__main__":
+    machine = AbstractMachine("test_object_code.loc")
+    machine.run()
