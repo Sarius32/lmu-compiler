3369093b0d95aa31743009471868ca5cc9134da3
Moritz Gärtner <moritzgaertner516@gmail.com>
2025-02-23 14:13:13 +0100

    nodes file added

---
+++ nodes.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ClassNode:
+    name: str
+    vars_: list
+    funcs: list
+
+
+@dataclass
+class VariableDef:
+    name: str
+    value: any
+
+
+@dataclass
+class VariableNode:
+    name: str
+
+
+@dataclass
+class FunctionDef:
+    name: str
+    args: list
+    vars_: list
+    stmts: list
+
+
+@dataclass
+class OperationNode:
+    left: any
+    operation: any
+    right: any
+
+
+@dataclass
+class AssignmentNode:
+    var: VariableNode
+    value: any
+
+
+@dataclass
+class FunctionCallNode:
+    name: str
+    args: list
+
+
+@dataclass
+class IfThenElseNode:
+    condition: any
+    then: list
+    alternative: list
+
+
+@dataclass
+class WhileNode:
+    condition: any
+    do: list
+
+
+@dataclass
+class InvertNode:
+    condition: any
+
+
+@dataclass
+class ComparisonNode:
+    left: any
+    operation: any
+    right: any
+
+
+@dataclass
+class InputNode:
+    ...
+
+
+@dataclass
+class WriteNode:
+    expr: any
