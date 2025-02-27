02e7d5c6623216fa61b632d3d91d0fdb2526cfea
Moritz Gärtner <moritzgaertner516@gmail.com>
2025-02-27 12:49:28 +0100

    tokens

---
+++ tokens.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class Identifier:
+    value: str
+
+
+@dataclass
+class Literal:
+    value: int
+
+
+class Keyword(Enum):
+    CLASS = "class"
+    THIS = "this"
+
+    VAR = "var"
+    ATTR = "attr"
+    INT = "int"
+
+    FUNC = "function"
+    METHOD = "method"
+    RETURN = "return"
+
+    IF = "if"
+    THEN = "then"
+    ELSE = "else"
+    WHILE = "while"
+    DO = "do"
+
+    PRINT = "print"
+    INPUT = "input"
+
+
+KEYWORDS = [kw.value for kw in Keyword]
+
+
+class Separator(Enum):
+    DOT = "."
+    COMMA = ","
+    SEMICOLON = ";"
+    L_ROUND = "("
+    R_ROUND = ")"
+    L_CURLY = "{"
+    R_CURLY = "}"
+    L_SQUARE = "["
+    R_SQUARE = "]"
+
+
+SEPARATORS = [s.value for s in Separator]
+
+
+class Operator(Enum):
+    EQUAL = "="
+    PLUS = "+"
+    MINUS = "-"
+    MULTIPLY = "*"
+    DIVIDE = "/"
+
+    EQUALS = "=="
+    SMALLER = "<"
+    SMALLER_EQ = "<="
+    GREATER = ">"
+    GREATER_EQ = ">="
+    NOT_EQUAL = "!="
+    NOT = "!"
+
+
+OPERATORS = [o.value[0] for o in Operator]
+
+Token = Identifier | Literal | Keyword | Separator | Operator
